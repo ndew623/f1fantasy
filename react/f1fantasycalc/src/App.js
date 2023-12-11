1833af2c7d399681ae1adfdfa1ce6f997560d97f
Your Name <you@example.com>
2023-12-11 17:30:46 -0500

    more ui. ui styling.

--- react/f1fantasycalc/src/App.js
+++ react/f1fantasycalc/src/App.js
@@ -3,23 +3,39 @@ import TestList from './reorderable-list/list.js';
 
 function App() {
   return (
-    /* TITLE BAR */
-    <div className="App d-flex flex-column">
+    <div className="d-flex flex-column">
+      {/* TITLE BAR */}
       <div className="flex-grow-1" style={{height: '5rem', backgroundColor: 'black'}}>
         <div className="ms-4 text-start">
           <span className="pageTitle" style={{color: 'red', fontStyle: 'italic'}}>F1</span>
           <span className="ms-3 pageTitle" style={{color: 'white'}}>Fantasy Calculator</span>
         </div>
       </div>
-      {/* SAVE AND LOAD BUTTONS */}
-      <div>
-        <button className="btn btn-lg btn-primary"><i className="bi-save"></i> Save</button>
-        <button className="btn btn-primary"><i className="bi-upload"></i> Load</button>
-      </div>
+      <div className="d-flex flex-column ms-2">
+        {/* SAVE AND LOAD BUTTONS */}
+        <div className="d-flex flow-row mt-3">
+          <button className="btn btn-lg btn-primary ms-2"><i className="bi-save"></i> Save</button>
+          <button className="btn btn-lg btn-primary ms-2"><i className="bi-upload"></i> Load</button>
+        </div>
 
-      {/* PREDICTED ORDER LIST */}
-      <div >
-        <TestList style={{width: '50px'}}/>
+        {/*Enter cost cap*/}
+        <h1 className="text-start mt-3">1. Enter cost cap</h1>
+        <div className="d-flex flex-row align-items-center CostCapInputLabelBold ms-4">
+          $<input type="text" className="form-control CostCapInput"/> <span class="ms-1">M</span>
+        </div>
+        {/*Calc swap penalties*/}
+        <h1 className="text-start mt-3">2. Calculate swap penalties</h1>
+        <div className="d-flex flex-column ms-4">
+          <div>
+            <input type="checkbox" className="form-check-input LargeCheckbox" id="includeSwapPenaltiesCheckbox"/>
+            <label className="form-check-label ms-2" for="includeSwapPenaltiesCheckbox" >Include swap penalties</label>
+          </div>
+
+        </div>
+        {/* PREDICTED ORDER LIST */}
+        <div >
+          <TestList style={{width: '50px'}}/>
+        </div>
       </div>
     </div>
   );
