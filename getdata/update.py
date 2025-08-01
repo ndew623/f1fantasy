3abdb5646f3babc2aab83ddbb8f07390f7730bda
Nick <dewdrop623@gmail.com>
2025-08-01 12:14:01 -0400

    update data

--- getdata/update.py
+++ getdata/update.py
@@ -2,7 +2,7 @@
 import json
 import urllib.request
 
-dataversion=13
+dataversion=14
 
 responseRaw = urllib.request.urlopen(f"https://fantasy.formula1.com/feeds/drivers/{dataversion}_en.json").read()
 response = json.loads(responseRaw)
